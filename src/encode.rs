2c8ceb62bed817815d2b386dea4dbcc92f350f40
lz1998 <875543533@qq.com>
2022-03-06 23:19:44 +0800

    rename

--- src/encode.rs
+++ src/encode.rs
@@ -4,81 +4,81 @@ use crate::{sdk, CMemory};
 
 pub fn encode_silk(
     src: Vec<u8>,
-    sampleRate: i32,
-    bitRate: i32,
+    sample_rate: i32,
+    bit_rate: i32,
     tencent: bool,
 ) -> Result<Vec<u8>, ()> {
-    unsafe { _encode_silk(src, sampleRate, bitRate, tencent) }
+    unsafe { _encode_silk(src, sample_rate, bit_rate, tencent) }
 }
 
 unsafe fn _encode_silk(
     src: Vec<u8>,
-    sampleRate: i32,
-    bitRate: i32,
+    sample_rate: i32,
+    bit_rate: i32,
     tencent: bool,
 ) -> Result<Vec<u8>, ()> {
-    let mut encControl = sdk::SKP_SILK_SDK_EncControlStruct {
-        API_sampleRate: sampleRate,
+    let mut enc_control = sdk::SKP_SILK_SDK_EncControlStruct {
+        API_sampleRate: sample_rate,
         maxInternalSampleRate: 24000,
-        packetSize: (20 * sampleRate) / 1000,
-        bitRate,
+        packetSize: (20 * sample_rate) / 1000,
+        bitRate: bit_rate,
         packetLossPercentage: 0,
         complexity: 2,
         useInBandFEC: 0,
         useDTX: 0,
     };
 
-    let mut encStatus = sdk::SKP_SILK_SDK_EncControlStruct {
+    let mut enc_status = sdk::SKP_SILK_SDK_EncControlStruct {
         API_sampleRate: 0,
         maxInternalSampleRate: 0,
         packetSize: 0,
-        bitRate,
+        bitRate: bit_rate,
         packetLossPercentage: 0,
         complexity: 0,
         useInBandFEC: 0,
         useDTX: 0,
     };
 
-    let mut encSizeBytes: i32 = 0;
-    if sdk::SKP_Silk_SDK_Get_Encoder_Size(&mut encSizeBytes) != 0 {
+    let mut enc_size_bytes: i32 = 0;
+    if sdk::SKP_Silk_SDK_Get_Encoder_Size(&mut enc_size_bytes) != 0 {
         return Err(());
     }
-    let psEnc = CMemory::new(encSizeBytes as usize);
+    let enc = CMemory::new(enc_size_bytes as usize);
 
     if sdk::SKP_Silk_SDK_InitEncoder(
-        psEnc.ptr,
-        &mut encStatus as *mut sdk::SKP_SILK_SDK_EncControlStruct,
+        enc.ptr,
+        &mut enc_status as *mut sdk::SKP_SILK_SDK_EncControlStruct,
     ) != 0
     {
         return Err(());
     }
-    let frameSize = sampleRate / 1000 * 40;
+    let frame_size = sample_rate / 1000 * 40;
     let mut out = BytesMut::new();
     if tencent {
         out.put_slice("\x02#!SILK_V3".as_bytes())
     } else {
         out.put_slice("#!SILK_V3".as_bytes())
     }
-    let mut nBytes: i16 = 1250;
-    let mut payload = vec![0; nBytes as usize];
-    for chunk in src.chunks(frameSize as usize) {
-        nBytes = 1250;
-        if chunk.len() < frameSize as usize {
+    let mut n_bytes: i16 = 1250;
+    let mut payload = vec![0; n_bytes as usize];
+    for chunk in src.chunks(frame_size as usize) {
+        n_bytes = 1250;
+        if chunk.len() < frame_size as usize {
             break;
         }
         let ret = sdk::SKP_Silk_SDK_Encode(
-            psEnc.ptr,
-            &mut encControl,
+            enc.ptr,
+            &mut enc_control,
             chunk.as_ptr() as *const i16,
             chunk.len() as i32 / 2,
             payload.as_mut_ptr(),
-            &mut nBytes,
+            &mut n_bytes,
         );
         if ret != 0 {
             return Err(());
         }
-        out.put_i16_le(nBytes);
-        let l = nBytes as usize;
+        out.put_i16_le(n_bytes);
+        let l = n_bytes as usize;
         out.put_slice(&payload[0..l]);
     }
 
