7c58920b56981561275228e3c377c02732f8bf8d
lz1998 <875543533@qq.com>
2022-03-06 18:13:10 +0800

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,20 @@ pub mod sdk {
 
 #[cfg(test)]
 mod tests {
+    use crate::sdk;
+
     #[test]
     fn it_works() {
+        unsafe {
+            a();
+        }
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+    unsafe fn a() {
+        let mut a: std::os::raw::c_int = 1;
+        let raw_mut = &mut a as *mut i32;
+        sdk::SKP_Silk_SDK_Get_Decoder_Size(raw_mut);
+        println!("{}", a);
     }
 }
