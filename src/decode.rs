d3381bea2347ccd76bd9583d90abb5e172c41de6
zkonge <zkonge@outlook.com>
2022-03-18 07:07:29 +0800

    Improve performance

--- src/decode.rs
+++ src/decode.rs
@@ -1,20 +1,26 @@
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::ffi::c_void;
 
-use crate::{sdk, SilkError};
+use bytes::Buf;
 
-pub fn decode_silk(src: Vec<u8>, sampleRate: i32) -> Result<Vec<u8>, SilkError> {
-    unsafe { _decode_silk(src, sampleRate) }
+use crate::{fast_check, sdk, SilkError};
+
+pub fn decode_silk(src: impl AsRef<[u8]>, sample_rate: i32) -> Result<Vec<u8>, SilkError> {
+    unsafe { _decode_silk(src.as_ref(), sample_rate) }
 }
 
-unsafe fn _decode_silk(src: Vec<u8>, sample_rate: i32) -> Result<Vec<u8>, SilkError> {
-    let mut src = Bytes::from(src);
-    if src.starts_with("#!SILK_V3".as_bytes()) {
-        src.advance("#!SILK_V3".len());
-    } else if src.starts_with("\x02#!SILK_V3".as_bytes()) {
-        src.advance("\x02#!SILK_V3".len());
+unsafe fn _decode_silk(mut src: &[u8], sample_rate: i32) -> Result<Vec<u8>, SilkError> {
+    // skip tencent flag
+    if src.starts_with(b"\x02") {
+        src.advance(1);
+    };
+
+    const SILK_HEADER: &[u8] = b"#!SILK_V3";
+    if src.starts_with(SILK_HEADER) {
+        src.advance(SILK_HEADER.len());
     } else {
-        return Err(SilkError::INVALID);
-    }
+        return Err(SilkError::Invalid);
+    };
+
     let mut dec_control = sdk::SKP_SILK_SDK_DecControlStruct {
         API_sampleRate: sample_rate,
         frameSize: 0,
@@ -22,45 +28,45 @@ unsafe fn _decode_silk(src: Vec<u8>, sample_rate: i32) -> Result<Vec<u8>, SilkEr
         moreInternalDecoderFrames: 0,
         inBandFECOffset: 0,
     };
-    let mut dec_size = 0;
-    let code = sdk::SKP_Silk_SDK_Get_Decoder_Size(&mut dec_size);
-    if code != 0 {
-        return Err(SilkError::from(code));
-    }
-    let mut dec = vec![0u8; dec_size as usize];
-    let code = sdk::SKP_Silk_SDK_InitDecoder(dec.as_mut_ptr() as *mut std::os::raw::c_void);
-    if code != 0 {
-        return Err(SilkError::from(code));
-    }
+
+    let mut decoder_size = 0;
+    fast_check!(sdk::SKP_Silk_SDK_Get_Decoder_Size(&mut decoder_size));
+
+    let mut decoder = vec![0u8; decoder_size as usize];
+    fast_check!(sdk::SKP_Silk_SDK_InitDecoder(
+        decoder.as_mut_ptr() as *mut c_void
+    ));
+
+    let mut result = vec![];
     let frame_size = sample_rate as usize / 1000 * 40;
-    let mut buf: Vec<u8> = vec![0; frame_size];
-    let mut out = BytesMut::new();
+    let mut buf = vec![0u8; frame_size];
     loop {
         if src.remaining() < 2 {
             break;
         }
-        let mut n_bytes = src.get_i16_le();
-        if n_bytes > frame_size as i16 {
-            return Err(SilkError::INVALID);
+        let input_size = src.get_i16_le();
+        if input_size > frame_size as i16 {
+            return Err(SilkError::Invalid);
         }
-        if src.remaining() < n_bytes as usize {
-            return Err(SilkError::INVALID);
+        if src.remaining() < input_size as usize {
+            return Err(SilkError::Invalid);
         }
-        let input = src.copy_to_bytes(n_bytes as usize);
-        let code = sdk::SKP_Silk_SDK_Decode(
-            dec.as_mut_ptr() as *mut std::os::raw::c_void,
+
+        let input;
+        (input, src) = src.split_at(input_size as usize);
+
+        let mut output_size = 0i16;
+        fast_check!(sdk::SKP_Silk_SDK_Decode(
+            decoder.as_mut_ptr() as *mut c_void,
             &mut dec_control,
             0,
             input.as_ptr(),
-            n_bytes as i32,
+            input_size as i32,
             buf.as_mut_ptr() as *mut i16,
-            &mut n_bytes,
-        );
-        if code != 0 {
-            return Err(SilkError::from(code));
-        }
-        let l = n_bytes as usize * 2;
-        out.put_slice(&buf[0..l])
+            &mut output_size,
+        ));
+
+        result.extend_from_slice(&buf[0..output_size as usize * 2])
     }
-    Ok(out.to_vec())
+    Ok(result)
 }
